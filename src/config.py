af6aae5d161d50c817d59087c3735422b9facb23
amirabdi <amir.h.abdi@gmail.com>
2019-02-13 16:00:44 -0800

    Create output directory

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
+import os
 from pathlib import Path
 
-root_path = str(Path.cwd() / '..')
+root_path = str(Path.cwd() / '../output')
+os.makedirs(str(root_path), exist_ok=True)
 
 trained_directory = Path(root_path) / 'trained'
 tensorboard_log_directory = Path(root_path) / 'logs_tb'
